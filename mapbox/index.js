0851e63bf4d6ba532c7d910f286d8a604e0e018e
robin-qu <hongbinqu9@gmail.com>
2019-03-07 21:34:56 -0800

    merge html and js

--- mapbox/index.js
+++ mapbox/index.js
@@ -1,160 +1,146 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoieGllZ3Vkb25nNDU2IiwiYSI6ImNqczUyczJ1NzBiZDM0NG5peDV1MGc0OHgifQ.zqfiaGVaYRrjY--NLs9kxw';
 
-var bounds = [
-	[-122.5063, 47.4849], // Southwest coordinates
-	[-122.0877, 47.7425]  // Northeast coordinates
-];
-
 var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v9',
-	center: [-122.3323077, 47.610582],
-	zoom: 11,
-	maxBounds: bounds
+    center: [237.66921043396, 47.604889909932865],
+    zoom: 10
 });
 
-map.on('load', () => {
-	// add hospitals
-	map.addSource('hos', {
-		type: 'geojson',
-		data: 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/shenghao/external_data/Hospitals.geojson',
 
-	});
+// Datasets
+var sidewalks = 'https://raw.githubusercontent.com/robin-qu/City-scale-Analytics/' +
+    'master/data_table/sidewalks.geojson'
+var crossings = 'https://raw.githubusercontent.com/robin-qu/City-scale-Analytics/' +
+    'master/data_table/crossings.geojson'
+var hospitals = 'https://raw.githubusercontent.com/robin-qu/City-scale-Analytics/' +
+    'master/data_table/Hospitals.geojson'
+var dog_off_leash_areas = 'https://raw.githubusercontent.com/robin-qu/City-scale-Analytics/' +
+    'master/data_table/Seattle%20Parks%20and%20Recreation%20GIS%20Map%20Layer' +
+    '%20Shapefile%20-%20Dog%20Off%20Leash%20Areas.geojson'
+var drinking_fountains = 'https://raw.githubusercontent.com/robin-qu/City-scale-Analytics/' +
+    'master/data_table/Seattle%20Parks%20and%20Recreation%20GIS%20Map%20Layer%20' +
+    'Shapefile%20-%20Drinking%20Fountain.geojson'
+var public_restrooms = 'https://raw.githubusercontent.com/robin-qu/City-scale-Analytics/master/data_table/' +
+    'Seattle%20Parks%20and%20Recreation%20GIS%20Map%20Layer%20Shapefile%20-%20' +
+    'Public%20Restroom.geojson'
+var view_points = 'https://raw.githubusercontent.com/robin-qu/City-scale-Analytics/master/data_table/' +
+    'Seattle%20Parks%20and%20Recreation%20GIS%20Map%20Layer%20Shapefile%20-%20View%20Points.geojson'
+var walkshed = 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/' +
+    'shenghao/walkshed%20test/test_walkshed.geojson'
 
-	map.addLayer({
-		id: 'Hospitals',
-		source: 'hos',
-		type: 'circle',
-		'layout': {
-			'visibility': 'visible'
-		},
-		paint: {
-			'circle-radius': 5,
-			'circle-color': 'black'
-		}
-	});
+// Add datasets
+map.on('load', function() {
+    function addLine(file, id, color, line_width) {
+        map.addLayer({
+            id: id,
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data: file
+            },
+            paint: {
+                'line-color': color,
+                'line-width': line_width
+            }
+        })
+    }
 
-	//add drinking_fountain
-	map.addSource('df', {
-		type: 'geojson',
-		data: 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/shenghao/external_data/Drinking%20Fountain.geojson'
-	});
+    function addPoints(file, source, id, color) {
+        map.addSource(source, {
+            type: "geojson",
+            data: file,
+            cluster: true,
+            clusterMaxZoom: 14,
+            clusterRadius: 50
+        });
 
-	map.addLayer({
-		id: 'Drinking Fountains',
-		source: 'df',
-		type: 'circle',
-		'layout': {
-			'visibility': 'visible'
-		},
-		paint: {
-			'circle-radius': 5,
-			'circle-color': '#f08'
-		}
-	});
+        map.addLayer({
+            id: id,
+            type: "circle",
+            source: source,
+            filter: ["has", "point_count"],
+            paint: {
+                "circle-color": color,
+                "circle-radius": [
+                    "step",
+                    ["get", "point_count"],
+                    20,
+                    50,
+                    30,
+                    200,
+                    40
+                ]
+            }
+        });
 
-	//public restroom
-	map.addSource('pr', {
-		type: 'geojson',
-		data: 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/shenghao/external_data/Public%20Restroom.geojson'
+        map.addLayer({
+            id: "unclustered " + id,
+            type: "circle",
+            source: source,
+            filter: ["!", ["has", "point_count"]],
+            paint: {
+                "circle-color": color,
+                "circle-radius": 5,
+                "circle-stroke-width": 1,
+                "circle-stroke-color": "#000000"
+            }
+        });
 
-	});
+        map.addLayer({
+            id: id + "text",
+            type: "symbol",
+            source: source,
+            filter: ["has", "point_count"],
+            layout: {
+                "text-field": "{point_count_abbreviated}",
+                "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+                "text-size": 12
+            }
+        });
 
-	map.addLayer({
-		id: 'Public Restrooms',
-		source: 'pr',
-		type: 'circle',
-		'layout': {
-			'visibility': 'visible'
-		},
-		paint: {
-			'circle-radius': 5,
-			'circle-color': 'green'
-		}
-	});
+        // inspect a cluster on click
+        map.on('click', id, function (e) {
+            var features = map.queryRenderedFeatures(e.point, { layers: [id] });
+            var clusterId = features[0].properties.cluster_id;
+            map.getSource(source).getClusterExpansionZoom(clusterId, function (err, zoom) {
+                if (err)
+                    return;
 
-	// dog off leash
-	map.addSource('dol', {
-		type: 'geojson',
-		data: 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/shenghao/external_data/Dog%20Off%20Leash%20Areas.geojson'
+                map.easeTo({
+                    center: features[0].geometry.coordinates,
+                    zoom: zoom
+                });
+            });
+        });
 
-	});
+        map.on('mouseenter', id, function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', id, function () {
+            map.getCanvas().style.cursor = '';
+        });
+    }
 
-	map.addLayer({
-		id: 'Dog Off Leash Areas',
-		source: 'dol',
-		type: 'circle',
-		'layout': {
-			'visibility': 'visible'
-		},
-		paint: {
-			'circle-radius': 5,
-			'circle-color': '#F08080'
-		}
-	});
-
-	//public art
-	// map.addSource('art', {
-	// 	type: 'geojson',
-	// 	data: 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/shenghao/external_data/public_arts.geojson'
-	//
-	// });
-	//
-	// map.addLayer({
-	// 	id: 'Public Arts',
-	// 	source: 'art',
-	// 	type: 'circle',
-	// 	paint: {
-	// 		'circle-radius': 5,
-	// 		'circle-color': '#1313D1'
-	// 	}
-	// });
-
-	//walkshed
-	map.addLayer({
-		id: 'walkshed',
-		type: 'line',
-		source: {
-			type: 'geojson',
-			data: 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/shenghao/walkshed%20test/test_walkshed.geojson'
-		},
-		paint: {
-			"line-color": "#000000",
-			"line-width": 4
-		}
-	});
-
-	// sidewalk
-	map.addLayer({
-		id: 'sidewalk',
-		type: 'line',
-		source: {
-			type: 'geojson',
-			data: 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/shenghao/18%20AU/data_table/sidewalks.geojson'
-		},
-		paint: {
-			"line-color": "#005EFF",
-			"line-width": 1
-		}
-	});
-
-	// crossing
-	map.addLayer({
-		id: 'crossing',
-		type: 'line',
-		source: {
-			type: 'geojson',
-			data: 'https://raw.githubusercontent.com/miss-grass/City-scale-Analytics/shenghao/18%20AU/data_table/crossings.geojson'
-		},
-		paint: {
-			"line-color": "#FF00FF",
-			"line-width": 1
-		}
-	})
-
+    //visualize
+    addLine(sidewalks, 'Sidewalks', '#000000', 1)
+    addLine(crossings, 'Crossings', '#FF0000', 1)
+    addLine(walkshed, 'Walkshed', '#0000FF', 6)
+    addPoints(dog_off_leash_areas, 'dola', 'Dog Off Leash Areas', '#006400')
+    addPoints(public_restrooms, 'pr', 'Public Restrooms', '#4B0082')
+    addPoints(drinking_fountains, 'df', 'Drinking Fountains', '#6495ED')
+    addPoints(view_points, 'vp', 'View Points', '#FFD700')
+    addPoints(hospitals, 'hsp', 'Hospitals', '#FF1493')
 });
 
-var toggleableLayerIds = ['Drinking Fountains', 'Dog Off Leash Areas', 'Hospitals', 'Public Restrooms'];
+
+// Filter features by toggling a list
+var marker;
+var toggleableLayerIds = ['Drinking Fountains',
+                          'Dog Off Leash Areas',
+                          'Hospitals',
+                          'Public Restrooms',
+                          'View Points'];
 
 for (var i = 0; i < toggleableLayerIds.length; i++) {
 	var id = toggleableLayerIds[i];
@@ -163,9 +149,13 @@ for (var i = 0; i < toggleableLayerIds.length; i++) {
 	link.href = '#';
 	link.className = 'active';
 	link.textContent = id;
+    link.uncluster = "unclustered " + id;
+    link.numbertext = id + "text";
 
 	link.onclick = function (e) {
 		var clickedLayer = this.textContent;
+        var clickedLayer2 = this.uncluster;
+        var clickedLayer3 = this.numbertext;
 		e.preventDefault();
 		e.stopPropagation();
 
@@ -173,31 +163,48 @@ for (var i = 0; i < toggleableLayerIds.length; i++) {
 
 		if (visibility === 'visible') {
 			map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            map.setLayoutProperty(clickedLayer2, 'visibility', 'none');
+            map.setLayoutProperty(clickedLayer3, 'visibility', 'none');
 			this.className = '';
 		} else {
 			this.className = 'active';
 			map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+            map.setLayoutProperty(clickedLayer2, 'visibility', 'visible');
+            map.setLayoutProperty(clickedLayer3, 'visibility', 'visible');
 		}
 	};
 
 	var layers = document.getElementById('menu');
 	layers.appendChild(link);
 }
+
+
 // Add Zoom-in, Zoom-out button
 map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+
+// access current location
+map.addControl(new mapboxgl.GeolocateControl({
+	positionOptions: {
+		enableHighAccuracy: true
+	},
+	trackUserLocation: true
+}), 'bottom-left');
+
 
 //show lat and long in console
 map.on('click', function(e) {
 	var isClicked = false;
+
 	const result = map.queryRenderedFeatures(e.point, {layers:['sidewalk']});
-	if (!isClicked) {
-		if (result.length > 0) {
-			var marker = new mapboxgl.Marker({color: "red"}).setLngLat(e.lngLat).addTo(map);
-		}
-	} else {
+	if (!isClicked && result.length > 0) {
+		marker = new mapboxgl.Marker(e).setLngLat(e.lngLat).addTo(map);
+		isClicked = true;
+	} else if (isClicked) {
 		marker.remove();
+		isClicked = false;
 	}
-	isClicked = !isClicked;
+
 	console.log(isClicked);
 	console.log('click', e.lngLat);
 })
